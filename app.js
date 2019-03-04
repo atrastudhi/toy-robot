beba5a99bd8372c5b9ba60990701258d2cfdd2c3
atras <atras.r@gmail.com>
2019-03-04 15:52:03 +0700

    initial skeleton

---
+++ app.js
@@ -0,0 +1,24 @@
+const fs = require('fs')
+
+const command = process.argv.slice(2)
+
+switch (command[0]) {
+  case 'PLACE':
+  
+    break;
+  case 'MOVE':
+
+    break;
+  case 'LEFT':
+
+    break;
+  case 'RIGHT':
+  
+    break;
+  case 'REPORT':
+  
+    break;
+  default:
+    console.log('invalid input')
+    break;
+}
